579ca5112fb65cd3e1a761ffa90e46d9e6589259
河合桜汰 <o.k.20020929@icloud.com>
2023-07-26 03:05:45 +0900

    main部分など全てにおいてメニューバーの下に薄い青色の背景色をつけるためにコンポーネントを作成しました

---
+++ frontend/app/fct-app/components/Background.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from '../styles/background.module.css'
+
+const Background = () => {
+  return (
+    <div className={styles.container}></div>
+  )
+}
+
+export default Background
--- frontend/app/fct-app/pages/main.tsx
+++ frontend/app/fct-app/pages/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Menubar from '../components/Menubar';
+import Background from '../components/Background';
 
 const Main = () => {
   return (
     <>
-      <Menubar/>
+      <Menubar />
+      <Background/>
     </>
   );
 };
