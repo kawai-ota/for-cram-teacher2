06ce0640f041347ccdb8c9d77b9a76eceeb52773
河合桜汰 <o.k.20020929@icloud.com>
2023-08-05 01:31:15 +0900

    オープニングをつけました

--- frontend/fct-app/app/page.tsx
+++ frontend/fct-app/app/page.tsx
@@ -11,15 +11,11 @@ export default function Home() {
         <div className={styles.text_overlay}>
           <h1>For Cram Teacher</h1>
           <h2>〜全ての塾の先生へ〜</h2>
-          <Link href="/signin" legacyBehavior>
-            <a className={styles.button}>
-              <button>新規登録はこちら</button>
-            </a>
+          <Link href="/team">
+            <button className={styles.button}>新規登録はこちら</button>
           </Link>
-          <Link href="/login" legacyBehavior>
-            <a className={styles.button}>
-              <button>ログインはこちら</button>
-            </a>
+          <Link href="/main">
+            <button className={styles.button}>ログインはこちら</button>
           </Link>
         </div>
       </div>
