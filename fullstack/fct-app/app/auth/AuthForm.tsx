cb11c1d33a47c6d3768b02eb429ad6d2e626b867
河合桜汰 <o.k.20020929@icloud.com>
2023-08-17 23:24:16 +0900

    Googleログインはよくよく考えたら、ないほうがいいと感じたので削除した。UIの変更も多少した。

--- fullstack/fct-app/app/auth/AuthForm.tsx
+++ fullstack/fct-app/app/auth/AuthForm.tsx
@@ -141,20 +141,9 @@ const AuthForm = () => {
             <div className="absolute inset-0 flex items-center">
               <div className="w-full border-t border-gray-300" />
             </div>
-            <div className="relative flex justify-center text-sm">
-              <span className="bg-white px-2 text-gray-500">
-                Or continue with
-              </span>
-            </div>
-          </div>
-          <div className="mt-6 flex gap-2">
-            <AuthSocialButton
-              icon={BsGoogle}
-              onClick={() => socialAction("google")}
-            />
           </div>
         </div>
-        <div className="flex gap-2 justify-center text-sm mt-6 px-2 text-gray-500">
+        <div className="flex gap-2 justify-center text-sm mt-10 px-2 text-gray-500">
           <div>
             {variant === "LOGIN"
               ? "新規登録をしますか？"
