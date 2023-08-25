13e2bf404d7fba516708f096ff4dd44d05871120
河合桜汰 <o.k.20020929@icloud.com>
2023-08-25 10:53:12 +0900

    新たにjobページの大まかな外形を作成しました

--- fullstack/fct-app/app/job/page.tsx
+++ fullstack/fct-app/app/job/page.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import Image from "next/image";
 
-const page = () => {
-  return <div>page</div>;
+const Page = () => {
+  return (
+    <div className="flex flex-col h-screen">
+      <div className="bg-white h-[calc(36.22vh)] relative flex items-center justify-center flex-col">
+        <p className="text-5xl font-bold">For Cram Teacher</p>
+        <p className="text-xl mt-5">
+          先生方の活躍をなくさず、誰もが輝ける場所で仕事を。
+        </p>
+      </div>
+      <div className="h-screen bg-gray-100"></div>
+    </div>
+  );
 };
 
-export default page;
+export default Page;
