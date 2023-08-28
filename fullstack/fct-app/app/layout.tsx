933136b698728f92123dc34b755ed2779bd139da
河合桜汰 <o.k.20020929@icloud.com>
2023-08-28 22:45:51 +0900

    layout.tsxを修正して、toastを使用してバリデーションエラーが出るように設定した

--- fullstack/fct-app/app/layout.tsx
+++ fullstack/fct-app/app/layout.tsx
@@ -1,8 +1,8 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import ToasterContext from "./chatpage/context/ToasterContext";
 import AuthContext from "./chatpage/context/AuthContext";
+import ToasterContext from "./chatpage/context/ToasterContext";
 import ActiveStatus from "./chatpage/components/ActiveStatus";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -21,6 +21,7 @@ export default function RootLayout({
     <html lang="ja">
       <body className={inter.className}>
         <AuthContext>
+          <ToasterContext />
           <ActiveStatus />
           {children}
         </AuthContext>
