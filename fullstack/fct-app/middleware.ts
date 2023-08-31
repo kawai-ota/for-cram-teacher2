8cd267fc5cfaa950c13a5ff9ba5b5df6efc98337
河合桜汰 <o.k.20020929@icloud.com>
2023-09-01 00:15:24 +0900

    midlewareの修正を行いました

--- fullstack/fct-app/middleware.ts
+++ fullstack/fct-app/middleware.ts
@@ -7,11 +7,5 @@ export default withAuth({
 });
 
 export const config = {
-  matcher: [
-    "/main/:path*",
-    "/chatpage/users/:path*",
-    "/chatpage/conversations/:path*",
-    "/paidpage",
-    "/teampage/:path*",
-  ],
+  matcher: ["/chatpage/users/:path*", "/chatpage/conversations/:path*"],
 };
