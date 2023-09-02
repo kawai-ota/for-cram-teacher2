ce78aec34a132f93774193f86f26028869327bf8
河合桜汰 <o.k.20020929@icloud.com>
2023-09-03 04:31:34 +0900

    postモデルを追加しました

--- fullstack/fct-app/app/detail/components/Header.tsx
+++ fullstack/fct-app/app/detail/components/Header.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="position-sticky w-full h-12 bg-white flex items-center justify-center">
+    <div className="w-full h-12 bg-white flex items-center justify-center">
       <p className="font-bold text-gray-900">For Cram Teacher</p>
     </div>
   );
